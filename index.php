56ebbdc8a63f55d1b4d192b935f1b4aaa9e9941e
JuanCM <juancampuzano2356@gmail.com>
2024-11-18 15:57:22 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+require('logic.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora Operaciones Básicas</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Operaciones básicas</h1>
+</body>
+</html>
